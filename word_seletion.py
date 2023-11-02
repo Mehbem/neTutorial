4e194dbb6d437178226316d38322e430e81cd4f5
Tom Verner <tom.j.verner@gmail.com>
2023-11-02 17:55:42 -0400

    e

--- word_seletion.py
+++ word_seletion.py
@@ -1,15 +1,23 @@
 #req methods:
+
 import random
+import pygame as pg
+
+#keystroke identity (key.Q is assigned str value "Q")
+
+
 
 #defining the word bank to select from
 
 wordbank = ["kangaroo", "ocelot", "baboon", "swordfish", "duck"]
 
+#Retrieve a random word from wordbank
 
-random_word = random.choice(wordbank) #gets a random word from the wordbank
+random_word = random.choice(wordbank)
+
+#info needed abt word: length, possible spaces & where, if a letter occurs (how many times & where), 
 
 len_word = len(random_word) #checks the length of the word 
 
-letter_picked = input("Pick a letter ") #asks the user to pick a letter and takes input 
 
 if letter_picked in random_word:
