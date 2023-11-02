397060b520d2a6fdba9183a5c6fc3b1ea2268a71
Tom Verner <tom.j.verner@gmail.com>
2023-11-02 17:33:06 -0400

    ioseugh

--- word_seletion.py
+++ word_seletion.py
@@ -1,8 +1,10 @@
+#req methods:
+import random
 
-wordbank = ["kangaroo", "ocelot", "baboon", "swordfish", "duck"] #word bank that is available in the game
+#defining the word bank to select from
 
+wordbank = ["kangaroo", "ocelot", "baboon", "swordfish", "duck"]
 
-import random #importng a method 
 
 random_word = random.choice(wordbank) #gets a random word from the wordbank
 
@@ -11,4 +13,3 @@ len_word = len(random_word) #checks the length of the word
 letter_picked = input("Pick a letter ") #asks the user to pick a letter and takes input 
 
 if letter_picked in random_word:
-    print 
